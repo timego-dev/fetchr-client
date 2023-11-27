c37bd276cb3cb59d737dcb18185396004a129c00
Dragan Boris <draganboris@outlook.com>
2023-11-27 15:21:27 +0700

    FIx font

--- src/components/RenderBlock/ContentBlock5.tsx
+++ src/components/RenderBlock/ContentBlock5.tsx
@@ -8,6 +8,13 @@ import {
 import { Media } from "../../types/payload-types";
 import { serializeSlate } from "../../utils/slateSerialize";
 import { Button } from "../ui/button";
+import { Antonio } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Antonio({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "200", "600"],
+});
 
 export const ContentBlock5: FC<IContentBlock5> = (props) => {
   const { blockType, blockName, description, title, id, image } = props;
@@ -18,7 +25,11 @@ export const ContentBlock5: FC<IContentBlock5> = (props) => {
         <h3 className="text-[45px] leading-[52px] text-center">{title}</h3>
         <img src={(image as Media)?.url || ""} alt="" />
       </div>
-      <div className=" text-center text-[500px] leading-[648px] text-primary font-bold absolute bottom-[-200px] ">
+      <div
+        className={cn(
+          " text-center text-[500px] leading-[648px] text-primary font-bold absolute bottom-[-200px] ",
+          font.className,
+        )}>
         {serializeSlate(description || [])}
       </div>
     </div>
