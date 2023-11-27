5c0c392d58741f622be6017fd5758b5051546b08
Dragan Boris <draganboris@outlook.com>
2023-11-27 15:24:40 +0700

    FIx: CSS

--- src/components/RenderBlock/ContentBlock6.tsx
+++ src/components/RenderBlock/ContentBlock6.tsx
@@ -14,7 +14,7 @@ export const ContentBlock6: FC<IContentBlock6> = (props) => {
   const { blockType, blockName, title, id, image } = props;
 
   return (
-    <div className=" px-[200px]  p-20  md:flex-nowrap min-h-[784px] bg-bg-default overflow-hidden relative">
+    <div className=" px-[200px]  p-20 py-[200px]  md:flex-nowrap min-h-[784px] bg-bg-default overflow-hidden relative">
       <div className="flex flex-col gap-y-10 items-center justify-center z-10">
         <img src={(image as Media)?.url || ""} alt="" />
         <h3 className="text-[45px] leading-[52px]">{title}</h3>
